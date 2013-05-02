d9467625466a50a2309d3141b9b8256c630855c0
Brian Fontenot <fontenotbrian@gmail.com>
2013-05-02 18:03:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multiauth::Application.config.secret_token = '05c6699b5170b2d55ff45e24d259f3359aa70820f60d94b89cb5637290a3e801ce48f46ebba47ff58b86a00a085c23641f7d8b59d50f29d5af78e02be741e621'
